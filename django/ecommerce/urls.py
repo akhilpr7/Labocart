41a63b91ffc9c020c25a2537a49ca74e096f5131
adith r <adith.r@epixelsolutions.com>
2022-10-18 11:23:07 +0530

    new template for shop

--- django/ecommerce/urls.py
+++ django/ecommerce/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-	path("",views.Home.as_view(),name="home"),
+	path("user/",views.Home.as_view(),name="home"),
+	path("shop/",views.Shop.as_view(),name="shop"),
+	path("laboshop/",views.Shop.as_view(),name="laboshop"),
+
 ]
--- django/project/urls.py
+++ django/project/urls.py
@@ -5,7 +5,8 @@ from django.urls import path, include  # add this
 urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
     path("", include("apps.authentication.urls")), # Auth routes - login / register
-    path('',include('ecommerce.urls'),name='home'),
+    path('',include('ecommerce.urls')),
+   
 
     # ADD NEW Routes HERE
 
