ed1c33fcccf20f75a21766c87b3df4273bc606f9
Akhil <akhil.c@epixelsolutions.com>
2022-10-31 15:58:11 +0530

    Added env variables to django

--- django/project/settings.py
+++ django/project/settings.py
@@ -92,11 +92,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {   
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'labocart', 
-        'USER': 'labouser', 
-        'PASSWORD': '123',
-        'HOST': 'db',
-        'PORT': 5432,
+        'NAME': os.environ.get('POSTGRES_NAME'), 
+        'USER': os.environ.get('POSTGRES_USER'), 
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
 
