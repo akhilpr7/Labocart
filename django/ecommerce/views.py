18c83d75a4007354fbc3b84c407f8ce0e9192ed4
AKHIL P R <akhil.pr@epixelsolutions.com>
2022-11-14 11:07:44 +0530

    ::::::::::::bUG FIXED ON  USER PAYMENTS:::::::

--- django/ecommerce/views.py
+++ django/ecommerce/views.py
@@ -7,7 +7,7 @@ from django.db.models import Q
 from django.urls import reverse
 from ecommerce.models import HireModel, ProductsModel, CartModel,RequestsModel
 from .forms import AddStockForm, HireNowForm,Laboregisterform,PurchaseForm,AddPackageForm
-from apps.home.models import Category
+from apps.home.models import Category,AppliedJobs
 from .forms import UpdateStockForm,CheckoutForm,UpdatePackageForm
 from .models import HireModel, PackageModel, PurchaseModel, LabopaymentModel
 import datetime
@@ -527,15 +527,12 @@ class Userpayments(View):
 	def get(self, request,id, *args, **kwargs):
 		wallet_balance =request.user.wallet
 		user_id=id
-		# print(wallet_balance,"qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq")
-		# print(user_id,"qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq")
-		rate = LabopaymentModel.objects.filter(work_id=id).values_list("rate")[0][0]
+		rate = AppliedJobs.objects.filter(id=id).values_list("rate")[0][0]
 		if wallet_balance >= rate:
-			NewUserModel.objects.filter(username=request.user.username).update(wallet=wallet_balance-rate)
-			LabopaymentModel.objects.filter(work_id=id).update(status=1,amount=rate)
+			NewUserModel.objects.filter(username=request.user.username).update(wallet=wallet_balance-rate)	
 			RequestsModel.objects.filter(id=id).update(status=3)
 			messages.success(request,'Payment Successful')
-			return redirect('laboshop')
+			return redirect('jobrequests')
 		else:
 			messages.success(request,'Payment Failed')
 			return redirect('laboshop')
