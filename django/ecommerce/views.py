1ce8846622a6d9655e63335e44d14650a75024a1
adith r <adith.r@epixelsolutions.com>
2022-11-15 17:19:39 +0530

    ui fixes in membership card

--- django/ecommerce/views.py
+++ django/ecommerce/views.py
@@ -458,7 +458,7 @@ class LaboRegister(View):
 				# obj.save()
 				messages.success(request,"Success !")
 			# print(obj,"55555555555555")
-				return redirect('shop')
+				return redirect('active_service')
 			# else:
 			# 	print(form.errors)    
 			# 	return render(request, self.template, {'form': form})
@@ -493,7 +493,7 @@ class Labocategories(View):
 @method_decorator(login_required,name='dispatch')
 class ActiveServices(View):
 	def get(self, request, *args, **kwargs):
-		data = labourmodels.objects.filter(Q(username = self.request.user.username) & (Q(status=0) | Q(status=1) | Q(status=2)))
+		data = labourmodels.objects.filter(Q(username = self.request.user.username) & (Q(status=0) | Q(status=1) | Q(status=2))).order_by("id")
 		user = NewUserModel.objects.all()
 		context = {
 			"data" : data,
