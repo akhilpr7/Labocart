8babe5d01cc709069dc472b17f6debda0c3d6aff
AKHIL P R <akhil.pr@epixelsolutions.com>
2022-11-21 14:18:15 +0530

    ::::::::bug::::::

--- django/apps/home/views.py
+++ django/apps/home/views.py
@@ -502,7 +502,6 @@ class Labocategories2(View):
     def get(self, request, *args, **kwargs):
         data = Category.objects.all()
         job = jobmodel.objects.values_list('category', flat=True)
-        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",job)
         context = {
             "data": data,
             "job": job,
